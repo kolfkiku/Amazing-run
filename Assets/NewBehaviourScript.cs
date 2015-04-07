5fddaf2c63c3ba53814449dee9bd047f9ea326a2
kolfkiku <kolfkiku@gmail.com>
2015-04-07 19:45:03 +0700

    Commit 1st_project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2 (0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (0,10);
+	}
+}
